d011da5a0ba7005ce9ebb3bf6f64a356f902991b
Ulises Trujillo <calogeroboy@gmail.com>
2019-05-19 01:53:15 -0600

    gzip compression enabled

--- index.php
+++ index.php
@@ -5,9 +5,24 @@ $yii=dirname(__FILE__).'/yii/framework/yii.php';
 $config=dirname(__FILE__).'/protected/config/main.php';
 
 // remove the following lines when in production mode
-defined('YII_DEBUG') or define('YII_DEBUG',true);
+//defined('YII_DEBUG') or define('YII_DEBUG',true);
 // specify how many levels of call stack should be shown in each log message
-defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',6);
+//defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',6);
 
 require_once($yii);
-Yii::createWebApplication($config)->run();
+
+// enable gzip compression
+$app = Yii::createWebApplication($config);
+// attaching a handler to application start
+Yii::app()->onBeginRequest = function($event)
+{
+// starting output buffering with gzip handler
+return ob_start("ob_gzhandler");
+};
+// attaching a handler to application end
+Yii::app()->onEndRequest = function($event)
+{
+// releasing output buffer
+return ob_end_flush();
+};
+$app->run();
